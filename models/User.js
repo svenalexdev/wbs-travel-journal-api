e3ac81fc7c4fc51637686bf85e01bff56a125bb8
Jorge Paul <jserranopaul@gmail.com>
2025-05-21 13:17:34 +0200

    Dropped user model createdAt in favour of timestamps

--- models/User.js
+++ models/User.js
@@ -6,8 +6,7 @@ const userSchema = new Schema(
     firstName: { type: String, required: [true, 'Firstname is required'] },
     lastName: { type: String, required: [true, 'Lastname is required'] },
     email: { type: String, required: [true, 'Email image is required'], unique: true },
-    password: { type: String, required: [true, 'Password is required'], select: false },
-    createdAt: { type: Date, default: Date.now }
+    password: { type: String, required: [true, 'Password is required'], select: false }
   },
   {
     timestamps: true
