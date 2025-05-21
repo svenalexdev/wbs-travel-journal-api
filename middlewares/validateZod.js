e0020ed5f256c110adc4a42e56e31ea0d2461af4
Garrett Lander <garrett.jlander@gmail.com>
2025-05-21 13:45:22 +0200

    merged with Express 5 updates

--- middlewares/validateZod.js
+++ middlewares/validateZod.js
@@ -1,11 +1,8 @@
 import { z } from 'zod/v4';
-import ErrorResponse from '../utils/ErrorResponse.js';
 
-const validateZod = (zodSchema) => (req, res, next) => {
-    const { error } = zodSchema.safeParse(req.body);
-    return error
-        ? next(new ErrorResponse(z.prettifyError(error), 400))
-        : next();
+const validateZod = zodSchema => (req, res, next) => {
+  const { error } = zodSchema.safeParse(req.body);
+  return error ? next(new Error(z.prettifyError(error), { cause: 400 })) : next();
 };
 
 export default validateZod;
